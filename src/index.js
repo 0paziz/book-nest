3ab988dc310701fc0f24fa10da3db0df6fb5b4e4
omar <omar12@gmail.com>
2025-05-03 18:25:00 +0300

    updated

--- src/index.js
+++ src/index.js
@@ -6,22 +6,22 @@ import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import Description from './Components/Description/Description';
 import BookList from './Components/BookList/BookList';
-import { HashRouter, Routes, Route } from "react-router-dom";
-
+import { HashRouter } from "react-router-dom";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home/>}>
-          <Route path="none" element={<About />} />
-          <Route path="/book/:id" element={<Description />} />
-          <Route path="book" element={< BookList/>} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+  <HashRouter>
+  <Routes>
+    <Route path="/" element={<Home />}>
+      <Route path="none" element={<About />} />
+      <Route path="book/:id" element={<Description />} />
+      <Route path="book" element={<BookList />} />
+    </Route>
+  </Routes>
+</HashRouter>
+
   </React.StrictMode>
 );
 
